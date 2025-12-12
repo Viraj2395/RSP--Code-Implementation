e3e1c5e712ffd8233033e1bc3ca7188dfafc36aa
Viraj2395 <prajapativiraj86@gmail.com>
2025-12-13 00:12:41 +1030

    Binary Tree is done

--- Binary_Tree.cpp
+++ Binary_Tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class Node{
     public:
@@ -14,15 +15,32 @@ class Node{
 
 };
 
-int main(){
-    Node* firstNode = new Node(2);
-    Node* secondNode = new Node(3);
-    Node* thirdNode = new Node(4);
-    Node* fouthNode = new Node(5);
+static int idx = -1;
 
-    firstNode->left = secondNode;
-    firstNode->right = thirdNode;
-    secondNode->left = fouthNode;
+Node* buildTree(std::vector<int> preOrder){
+    idx++;
+
+    if (preOrder[idx] == -1)
+    {
+        return nullptr;
+    }
+    
+
+    Node* root = new Node(preOrder[idx]);
+    root->left = buildTree(preOrder);
+    root->right = buildTree(preOrder);
+    return root;
+} 
+
+
+int main(){
+    std::vector<int> preOrder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+
+    Node* root = buildTree(preOrder);
+    std::cout<<root->data<<std::endl;
+    std::cout<<root->left->data<<std::endl;
+    std::cout<<root->right->data<<std::endl;
+
 
     return 0;
 }
